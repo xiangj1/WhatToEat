ec0ad621ba8a302165134cd24f4a9147c9a74bde
Xiang Jiang <xiangj1@outlook.com>
2019-09-09 08:23:12 -0700

    header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,2 +1,20 @@
 import React from 'react'
+import withStyles from '@material-ui/core/styles/withStyles'
+import { Theme } from '@material-ui/core'
 
+const styles = (theme: Theme) => ({
+  root: {
+    display: 'flex',
+  }
+})
+
+
+const Header = ({ classes }) => {
+  return (
+    <div className={classes.root}>
+      What to eat
+    </div>
+  )
+}
+
+export default withStyles(styles)(Header)
