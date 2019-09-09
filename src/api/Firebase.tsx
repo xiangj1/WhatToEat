74cdcc3362e289399277792865788e38d687acee
Steven <xiangj1@outlook.com>
2019-09-09 13:27:30 -0700

    removed config

--- src/api/Firebase.tsx
+++ src/api/Firebase.tsx
@@ -6,19 +6,4 @@ import firebaseConfig from '../config/FirebaseConfig'
 
 firebase.initializeApp(firebaseConfig)
 
-// function isSignedIn() {
-//   return new Promise(resolve => {
-//     if (firebase.auth().currentUser) resolve(firebase.auth().currentUser)
-//     else {
-//       firebase.auth().onAuthStateChanged(user => {
-//         if (user) {
-//           resolve(user)
-//         } else {
-//           resolve(null)
-//         }
-//       })
-//     }
-//   })
-// }
-
 export default firebase
