a999111a86de1a3f13d2d4853b75ccdefa195e23
Steven <xiangj1@outlook.com>
2019-09-11 15:37:05 -0700

    CLient

---
+++ src/api/Client.ts
@@ -0,0 +1,58 @@
+import database from './Firestore'
+import auth from './Auth'
+
+interface Dish extends firebase.firestore.DocumentData {
+  id: string | null
+  name: string
+  tags: string[]
+}
+
+interface Client {
+  setUid(newUid: string): string
+  signIn(email: string, password: string): Promise<string>
+  signOut(): Promise<void>
+  addDish(dishInfo: Dish): Promise<string>
+  getDishList(): Promise<Dish[]>
+}
+
+function Client(): Client {
+  const collectionName = 'Dishes'
+  let uid = ''
+
+  function setUid(newUid: string): string {
+    uid = newUid
+    return uid
+  }
+
+  async function signIn(email: string, password: string): Promise<string> {
+    setUid(await auth.signIn(email, password))
+    return uid
+  }
+
+  async function signOut(): Promise<void> {
+    await auth.signOut()
+  }
+
+  async function addDish(dishInfo: Dish): Promise<string> {
+    if (!uid) throw Error('User Not Signed In')
+    return database.addItem(uid, collectionName, dishInfo)
+  }
+
+  async function getDishList(): Promise<Dish[]> {
+    if (!uid) throw Error('User Not Signed In')
+    const itemList = await database.getItemList(uid, collectionName)
+    return itemList.map(({ id, name, tags }: firebase.firestore.DocumentData) => {
+      return { id, name, tags }
+    })
+  }
+
+  return {
+    setUid,
+    signIn,
+    signOut,
+    addDish,
+    getDishList
+  }
+}
+
+export default Client
