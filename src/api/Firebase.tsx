d872a70a76d54f13daadda62101dbe3c8310c663
Steven <xiangj1@outlook.com>
2019-09-11 15:36:51 -0700

    Firestore

--- src/api/Firebase.tsx
+++
@@ -1,8 +0,0 @@
-import firebase from 'firebase/app'
-import 'firebase/auth'
-
-import firebaseConfig from '../config/FirebaseConfig'
-
-firebase.initializeApp(firebaseConfig)
-
-export default firebase
---
+++ src/api/Firestore.ts
@@ -0,0 +1,32 @@
+import firebase from './Firebase'
+import 'firebase/firestore'
+
+const firestore = firebase.firestore()
+
+async function addItem(uid: string, collectionName: string, value: firebase.firestore.DocumentData): Promise<string> {
+  const docRef: firebase.firestore.DocumentReference = await firestore
+    .collection('Users')
+    .doc(uid)
+    .collection(collectionName)
+    .add(value)
+  return docRef.id
+}
+
+async function getItemList(uid: string, collectionName: string): Promise<firebase.firestore.DocumentData[]> {
+  const itemList: firebase.firestore.DocumentData[] = []
+
+  const querySnapshot: firebase.firestore.QuerySnapshot = await firestore
+    .collection('Users')
+    .doc(uid)
+    .collection(collectionName)
+    .get()
+
+  querySnapshot.forEach((document: firebase.firestore.QueryDocumentSnapshot) => itemList.push(document.data()))
+
+  return itemList
+}
+
+export default {
+  addItem,
+  getItemList
+}
